da300f2b213ff5702ed520a22c2c5f17da6387fc
Jiuye-neko <740359023@qq.com>
2024-02-22 09:49:55 +0800

    Initial commit

---
+++ algorithms/threeNums.ts
@@ -0,0 +1,39 @@
+function threeSum(nums: number[]): number[][] {
+  nums = nums.sort((a, b) => a - b);
+
+  let ans: number[][] = [];
+  let len = nums.length;
+
+  for (let i = 0; i < len - 2; i++) {
+    let x = nums[i];
+    if (i > 0 && x === nums[i - 1]) {
+      continue;
+    }
+    let j = i + 1;
+    let k = len - 1;
+    while (j < k) {
+      let sum = x + nums[j] + nums[k];
+
+      if (sum < 0) {
+        j += 1;
+      } else if (sum > 0) {
+        k -= 1;
+      } else {
+        ans.push([x, nums[j], nums[k]]);
+
+        j += 1;
+        while (j < k && nums[j] === nums[j - 1]) {
+          j += 1;
+        }
+        k -= 1;
+        while (j < k && nums[k] === nums[k + 1]) {
+          k -= 1;
+        }
+      }
+    }
+  }
+
+  return ans;
+}
+
+threeSum([1, -1, -1, 0]);
